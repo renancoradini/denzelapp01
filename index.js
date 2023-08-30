5019b30d4eb82ffd528a8f456df237cba2151811
renancoradini <renancoradini@gmail.com>
2023-08-30 11:32:57 +0100

    commit2-30-08-capgeminlaptop

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ type: Sequelize.STRING
 
 // Note: using `force: true` will drop the table if it already exists
 User.sync({ force: true }) // Now the `users` table in the database corresponds to the model definition
-app.get('/', (req, res) => res.json({ message: 'Hello World denzel updated3333333' }))
+app.get('/', (req, res) => res.json({ message: 'Hello World denzel updated4444444' }))
 
 //Add a POST endpoint that contains the user in the request body:
 app.post('/user', async (req, res) => {
